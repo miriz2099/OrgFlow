5fd520405f7b2a3cf1e8daaedf326d485ac2e23d
miri <miriz2099@gmail.com>
2026-02-17 00:13:32 +0200

    Setup: Initialized Spring Boot project structure

---
+++ src/main/java/com/orgflow/orgflow/OrgflowApplication.java
@@ -0,0 +1,13 @@
+package com.orgflow.orgflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orgflow/orgflow/OrgflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgflow.orgflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
